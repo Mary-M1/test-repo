f35a4e625052075e89328e2e27ed97a11ab2e095
Mary-M1 <69441132+Mary-M1@users.noreply.github.com>
2020-08-10 15:01:04 +0900

    Add files via upload

---
+++ test.py
@@ -0,0 +1,10 @@
+Python 2.7.18 (v2.7.18:8d21aa21f2, Apr 19 2020, 20:48:48) 
+[GCC 4.2.1 Compatible Apple LLVM 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> test
+
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    test
+NameError: name 'test' is not defined
+>>> 
